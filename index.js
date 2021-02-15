2b5ce3baaedfefc703e66ee43f49b7d5dce4a6ce
github username <github email address>
2021-02-15 16:59:56 -0500

    danger zone

---
+++ index.js
@@ -0,0 +1,70 @@
+const url = "http://localhost:3000/comics"
+const comicBar = document.querySelector('.all-comics')
+const allComics = document.querySelector('#all-button')
+const genres = document.querySelector('#genre-button')
+const genreUrl = "http://localhost:3000/comedy_genres"
+
+allComics.addEventListener('click', fetchComicItems)
+
+function fetchComicItems(){
+    fetch(url)
+    .then(res => res.json())
+    // .then(data => console.log(data))
+    .then(data => data.forEach(item => displayComic(item)))
+}
+
+function displayComic(item){
+    const comicImg = document.createElement("img")
+    comicImg.src = item.image
+    comicImg.dataset.id = item.id
+    // console.log(comicImg)
+
+     comicBar.append(comicImg)
+}
+
+genres.addEventListener('click', fetchGenres)
+
+function fetchGenres(){
+    fetch(genreUrl)
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(data => data.forEach(genre => displayGenres(genre)))
+}
+
+const genreList = document.querySelector('#genre-list')
+function displayGenres(genre){
+    // console.log(genre)
+    const genreLi = document.createElement('li')
+    genreLi.dataset.id = genre.id
+    genreLi.innerText = genre.genre_name
+    genreList.append(genreLi)
+}
+
+comicBar.addEventListener('click', itemClick)
+
+function itemClick(event){
+    // console.log(event.target.dataset.id)
+    const itemId = event.target.dataset.id
+    fetch (`${url}/${itemId}`)
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(data => displayOneComic(data))
+}
+
+const comicDetail = document.querySelector('.comic-container')
+const comicImage = document.querySelector('.comic-image')
+const comicName = document.querySelector('.name')
+const comicVideo = document.querySelector('.video')
+const comicBio = document.querySelector('.bio')
+function displayOneComic(data){
+    // console.log(data)
+    comicDetail.dataset.id = data.id
+    comicImage.src = data.image
+    comicImage.alt = data.name
+    comicName.innerText = data.name
+    comicVideo.src = data.video
+    comicBio.innerText = data.bio
+
+}
+
+// fetchComicItems()
