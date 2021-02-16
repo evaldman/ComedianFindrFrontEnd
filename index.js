eb174ce19b55d75d89793cd0a3967c6ff0f8136b
github username <github email address>
2021-02-16 11:34:22 -0500

    progress

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const allComics = document.querySelector('#all-button')
 const genres = document.querySelector('#genre-button')
 const genreUrl = "http://localhost:3000/comedy_genres"
 
-allComics.addEventListener('click', fetchComicItems)
+allComics.addEventListener('click', fetchComicItems, {once:true})
 
 function fetchComicItems(){
     fetch(url)
     .then(res => res.json())
     // .then(data => console.log(data))
+    // .then(location.reload())
     .then(data => data.forEach(item => displayComic(item)))
 }
 
@@ -22,18 +23,25 @@ function displayComic(item){
      comicBar.append(comicImg)
 }
 
-genres.addEventListener('click', fetchGenres)
+genres.addEventListener('click', fetchGenres, {once:true})
 
 function fetchGenres(){
     fetch(genreUrl)
     .then(response => response.json())
     // .then(data => console.log(data))
     .then(data => data.forEach(genre => displayGenres(genre)))
+    // reload()
+}
+
+function removeAll(){
+    document.getElementsByTagName("div").innerHTML = "";
 }
 
 const genreList = document.querySelector('#genre-list')
 function displayGenres(genre){
     // console.log(genre)
+    // location.reload()
+    // removeAll()
     const genreLi = document.createElement('li')
     genreLi.dataset.id = genre.id
     genreLi.innerText = genre.genre_name
@@ -44,11 +52,13 @@ comicBar.addEventListener('click', itemClick)
 
 function itemClick(event){
     // console.log(event.target.dataset.id)
+    if (event.toElement.localName === 'img'){
     const itemId = event.target.dataset.id
     fetch (`${url}/${itemId}`)
     .then(response => response.json())
     // .then(data => console.log(data))
     .then(data => displayOneComic(data))
+    }
 }
 
 const comicDetail = document.querySelector('.comic-container')
